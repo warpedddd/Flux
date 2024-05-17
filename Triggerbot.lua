79fa6a7b3e437378221870993f21729de46a767e
warpedd <110729110+warpedddd@users.noreply.github.com>
2024-05-17 17:18:19 +0300

    Update and rename Triggerbot to Triggerbot.lua

--- Triggerbot.lua
+++ Triggerbot.lua
@@ -3,7 +3,8 @@ getgenv().Triggerbot = {
     Enabled = true,
     Keybind = "V",
     Visuals = true,
-    Prediction = 0.11
+    Prediction = 0.11,
+    SendNotifications = true
 }
 
 -- // Services // --
@@ -24,18 +25,18 @@ local Field_Of_View    = Drawing.new("Circle");
 local GetClosestPlr; GetClosestPlr = function()
     local ClosestTarget = nil;
     local MaxDistance   = math.huge;
-    -- // Loops through player list // --
+    -- // Loops through the player list // --
     for value, index in pairs(Players:GetPlayers()) do
         -- // Excludes the local player from the for loop // --
         if index.Character and index ~= Local_Player and index.Character:FindFirstChild("HumanoidRootPart") then
             -- // Gets distance // --
             local Position, OnScreen = Current_Camera:WorldToScreenPoint(index.Character.HumanoidRootPart.Position)
             local Distance = (V2(Position.X, Position.Y) - V2(Get_Mouse_Pos.X, Get_Mouse_Pos.Y)).Magnitude
-            -- // Stops if player is not OnScreen ( Checks if the pivot is not available ) // --
+            -- // Stops if the player is not OnScreen ( Checks if the pivot is not available ) // --
             if (OnScreen ~= true) then
                 continue;
             end
-            -- // If the distance is less then math.huge // --
+            -- // If the distance is less than math.huge // --
             if (Distance < MaxDistance) then
                 MaxDistance = Distance
                 ClosestTarget = index
@@ -57,11 +58,11 @@ local GetClosestPlr2; GetClosestPlr2 = function()
             -- // Gets distance // --
             local Position, OnScreen = Current_Camera:WorldToScreenPoint(index.Character.HumanoidRootPart.Position)
             local Distance = (V2(Position.X, Position.Y) - V2(Get_Mouse_Pos.X, Get_Mouse_Pos.Y)).Magnitude
-            -- // Stops if player is not OnScreen ( Checks if the pivot is not available ) // --
+            -- // Stops if the player is not OnScreen ( Checks if the pivot is not available ) // --
             if (OnScreen ~= true) then
                 continue;
             end
-            -- // If the distance is less then math.huge and the field of view radius is greater than the distance // --
+            -- // If the distance is less than math.huge and the field of view radius is greater than the distance // --
             if (Field_Of_View.Radius > Distance and Distance < MaxDistance) then
                 MaxDistance = Distance
                 ClosestTarget = index
@@ -78,11 +79,11 @@ local Toggle; UserInputService.InputBegan:Connect(function(input, Processed)
         -- // Returns nil if already processing // --
         return
     end
-    -- // Checks if the key is pressed and if the triggerbot is enabled // --
+    -- // Checks if the key is pressed and if the trigger-bot is enabled // --
     if (input.KeyCode == Enum.KeyCode[getgenv().Triggerbot.Keybind] and getgenv().Triggerbot.Enabled) then
-        -- // Each press makes toggle the oppisite of the current boolean value // --
+        -- // Each press makes toggle the opposite of the current boolean value // --
         Toggle = not Toggle
-        -- // If its true it turns on the triggerbot // --
+        -- // If it is true it turns on the trigger-bot // --
         if (Toggle == true) then
             Triggerbot = true
             -- // Notifies the local player // --
@@ -98,7 +99,7 @@ local Toggle; UserInputService.InputBegan:Connect(function(input, Processed)
                 ) 
             end
         else
-            -- // If toggle is not true it will turn off the triggerbot // --
+            -- // If the toggle is not true it will turn off the trigger-bot // --
             Triggerbot = false
             -- // Notifies the local player // --
             if (getgenv().Triggerbot.SendNotification) then
@@ -127,24 +128,24 @@ local Run_Service = G:GetService("RunService"); Run_Service.RenderStepped:Connec
     Field_Of_View.Color  = Color3.new(255,255,255)
     Field_Of_View.Filled = false
 
-    -- // Checks if triggerbot is enabled and a target is found // --
+    -- // Checks if trigger-bot is enabled and a target is found // --
     if (Target[1] and  Target[1].Character and Triggerbot) then
         -- // Variable that determines the position of the closest player // --
         local Position = Current_Camera:WorldToScreenPoint(Target[1].Character.HumanoidRootPart.Position + Target[1].Character.HumanoidRootPart.Velocity * getgenv().Triggerbot.Prediction)
-        -- // Sets position of the field of view as the closest player // --
+        -- // Sets the position of the field of view as the closest player // --
         Field_Of_View.Position = V2(Position.X, Position.Y + 35)
 
-        -- // If triggerbot visuals is on the field of view is visible // --
+        -- // If trigger-bot visuals is on the field of view is visible // --
         if (getgenv().Triggerbot.Visuals) then
             Field_Of_View.Visible = true
         end
 
-        -- // If cursor is inside of the fov it will start clicking (mouse1press and mouse1click is broken on solara) // -- 
+        -- // If the cursor is inside of the field of view it will start clicking (mouse1press and mouse1click are broken on Solara) // -- 
         if (Target[2] and Target[2].Character) then
             game:GetService("VirtualUser"):ClickButton1(V2());
         end
     else
-        -- // Turns the fov non visible if no target is found // --
+        -- // Turns the field of view non-visible if no target is found // --
         Field_Of_View.Visible = false
     end
 end)
